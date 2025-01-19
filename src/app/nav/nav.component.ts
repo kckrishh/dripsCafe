7eb66551d6fd4487d7ba39e1e99cdddd5c3a9e48
Krish KC <khatrikrish03@gmail.com>
2025-01-19 01:28:20 -0500

    checking the responsiveness

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
@@ -8,4 +8,11 @@ import { MatSidenav } from '@angular/material/sidenav';
 })
 export class NavComponent {
   isOpen = false;
+
+  isScrolled = false;
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    this.isScrolled = scrollTop > 50;
+  }
 }
